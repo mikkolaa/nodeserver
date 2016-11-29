b2ea65bfc0ea6cf8c60469ff1b53c3dc7da741f2
Mikko Laanti <mikko.laanti@gmail.com>
2016-11-29 18:50:52 +0200

    Create sqldataserver.js
    SQL Serverin data Json:ksi

---
+++ sqldataserver.js
@@ -0,0 +1,37 @@
+/* Ladataan Express-modulit */
+var express = require('express');
+var app = express();
+
+/* Requestin käsittely */
+app.get('/', function (req, res) {
+	/* Driverin installointi: npm install mssql	*/
+  /* Katso, että palvelimella on SQL Server Browser päällä */
+    var sql = require("mssql");
+
+	/* Connection-konfiguraatio */
+    var config = {
+        user: 'sa',
+        password: 'xxxxxxx',
+        server: 'MILAX-PC\\SQLEXPRESS', 
+        database: 'Northwind',
+		port: 1433 						
+    };
+    
+    sql.connect(config, function (err) {    
+		if (err) 
+			console.log(err);
+
+		var request = new sql.Request();           
+		
+		request.query('select * from Customers', function (err, recordset) {            
+            if (err) 
+				console.log(err)
+            /* Data streamiin */
+            res.send(recordset);            
+        });
+    });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running in URL: http://localhost:5000');
+});
